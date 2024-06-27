87509f77c5aec38c210a935740b918cd654be192
Abeermohsien <aboorakhalil993@gmail.com>
2024-06-27 09:14:18 -0700

    es6 classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,12 @@
+/**
+ * a class room.
+ */
+export default class ClassRoom {
+  /**
+   * Creates @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - max size.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
