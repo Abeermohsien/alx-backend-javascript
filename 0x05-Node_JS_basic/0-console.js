fa329fb281061ed3f0de54d4b3d7138f155eb6b9
Abeermohsien <aboorakhalil993@gmail.com>
2024-08-20 10:26:21 -0700

    $

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * returning  message on the STDOUT
+ * @param {String} The message
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
